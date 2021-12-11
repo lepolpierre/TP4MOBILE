c1bff5927737d77c72a3b7de2a082cab7625fdfe
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2021-12-11 00:37:26 -0500

    11/12/2021

--- app/src/main/java/com/example/tp4mobilegwenaelgalliot/ui/Magasin/MagasinFragment.java
+++ app/src/main/java/com/example/tp4mobilegwenaelgalliot/ui/Magasin/MagasinFragment.java
@@ -1,28 +1,49 @@
 package com.example.tp4mobilegwenaelgalliot.ui.Magasin;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
-
-import com.example.tp4mobilegwenaelgalliot.ObjetAdapteur;
+import com.example.tp4mobilegwenaelgalliot.ui.ObjetAdapteur;
 import com.example.tp4mobilegwenaelgalliot.databinding.FragmentVendeurBinding;
 import com.example.tp4mobilegwenaelgalliot.databinding.VendeurRowBinding;
 import com.example.tp4mobilegwenaelgalliot.model.Objet;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class MagasinFragment extends Fragment {
 
-    private VendeurRowBinding binding;
+public class  MagasinFragment extends Fragment {
+
+    private FragmentVendeurBinding binding;
+
+    private static final String KEY_NAME = "OBJET";
+    private static final String KEY_CAT = "CATEGORIE";
+    private static final String KEY_PRIX = "PRIX";
+    private static final String KEY_QTT = "QUANTITE";
+    private String last;
 
     // Instanciation de la base de donnee firestore
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -42,15 +63,91 @@ public class MagasinFragment extends Fragment {
     private ObjetAdapteur objetAdapteur;
 
 
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentVendeurBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        objetAdapteur = new ObjetAdapteur(getActivity(), items) {
-        };
+        binding = FragmentVendeurBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        return view;
+    }
+
+
+
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.rvVendeur.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        objetAdapteur = new ObjetAdapteur(getActivity(),items);
+
         binding.rvVendeur.setAdapter(objetAdapteur);
-        return root;
+
+
+
+
+//        Map<String, Object> objet = new HashMap<>();
+//        objet.put(KEY_NAME, "banane");
+//        objet.put(KEY_CAT, "fruit");
+//        objet.put(KEY_PRIX, "11.0");
+//        objet.put(KEY_QTT, "2");
+//        collection
+//                .add(objet)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference DocumentReference) {
+//                        Log.d("TAG", "DocumentSnapshot added with ID: ");
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w("TAG", "Error adding document", e);
+//                    }
+//                });
+
+    }
+
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        /**
+         * Instanciation du listener : à collection donnée (ici, triée par priorité)
+         * Toute modification dans cette collection de la BD sera détéctée
+         * Traitée dans onEvent
+         */
+        registration = collection.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                Log.d("TAG", "onEvent: " + value);
+                items.clear();
+                for (QueryDocumentSnapshot document : value ) {
+                    Log.d("TAG", document.getId() + " => " + document.getData());
+                    Objet objet = new Objet();
+                    objet.setNom(document.getString(KEY_NAME));
+                    objet.setCategorie(document.getString(KEY_CAT));
+                    objet.setPrix(Double.parseDouble(document.getString(KEY_PRIX)));
+                    objet.setQtt(Double.parseDouble(document.getString(KEY_QTT)));
+
+
+                    // Ajout de l'Id créé par Firestore
+//                    objet.setId(document.getId());
+//                    last = document.getId();
+                    last = document.getId();
+                    items.add(objet);
+
+                }
+                objetAdapteur.notifyDataSetChanged();
+                Toast toast = Toast.makeText(getContext(), items.toString(), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/tp4mobilegwenaelgalliot/ui/ObjetAdapteur.java
+++ app/src/main/java/com/example/tp4mobilegwenaelgalliot/ui/ObjetAdapteur.java
@@ -20,14 +20,24 @@ import com.example.tp4mobilegwenaelgalliot.model.Objet;
 import java.util.ArrayList;
 import java.util.List;
 
-class ObjetAdapteur extends ArrayAdapter<Objet> {
+public class ObjetAdapteur extends RecyclerView.Adapter<ObjetAdapteur.NoteHolder> {
 
     private Context contexte;
-    private ArrayList<Objet> rowModels;
+    private ArrayList<Objet> objetsList;
     VendeurRowBinding binding;
 
-    public ObjetAdapteur(@NonNull Context context, int resource) {
-        super(context, resource);
+
+    public ObjetAdapteur(Activity context, ArrayList<Objet> m_RowModels) {
+            /* Le constructeur à 4 paramètres :
+               1. Activité appelante
+               2. Layout pour la rangée
+               3. Conteneur pour l'affichage du texte dans le layout
+               4. Données
+               */
+        //super(context, R.layout.vendeur_row, R.id.nom_rangee, m_RowModels);
+
+        contexte = context;
+        objetsList = m_RowModels;
     }
 
     @NonNull
@@ -43,16 +53,16 @@ class ObjetAdapteur extends ArrayAdapter<Objet> {
 
     @Override
     public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
-
         // recuperation du point
-        Objet current = rowModels.get(position);
+        Objet current = objetsList.get(position);
 
-        holder.txtAdress.setText(current.getNom());
-        holder.txtAdress.setText(current.get());
-        holder.txtCategorie.setText(current.getCategorie());
+        holder.tvCategorie.setText(current.getCategorie());
+        holder.tvNom.setText(current.getNom());
+        holder.tvPrix.setText(current.getPrix().toString());
+        holder.tvQtt.setText(current.getQtt().toString());
 
 
-        // set image
+//        // set image
 //        if (current.getCategorie().equals("parc")){
 //            holder.img.setImageResource(R.drawable.parc);
 //        }else if(current.getCategorie().equals("maison")){
@@ -60,42 +70,95 @@ class ObjetAdapteur extends ArrayAdapter<Objet> {
 //        }else{
 //            holder.img.setImageResource(R.drawable.foret);
 //        }
-
-        // gestion du clic sur la ranger
-        // Navigation vers le fragment Détail avec Id du point détaillé
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavDirections action = (NavDirections) HomeFragmentDirections.actionNavHomeToDetailsFragment(current.getId());
-                Navigation.findNavController(view).navigate(action);
-            }
-        });
+//
+//        // gestion du clic sur la ranger
+//        // Navigation vers le fragment Détail avec Id du point détaillé
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavDirections action = (NavDirections) HomeFragmentDirections.actionNavHomeToDetailsFragment(current.getId());
+//                Navigation.findNavController(view).navigate(action);
+//            }
+//        });
     }
 
-    // nombre d objet dans la liste
     @Override
     public int getItemCount() {
-        if (rowModels != null)
-            return rowModels.size();
-        else return 0;
+        return 0;
     }
 
-    // set location
-    public void setLocations(ArrayList<Objet> objet) {
-        rowModels = objet;
-        notifyDataSetChanged();
-    }
-
-
-    // référence les éléments de la vue
     public class NoteHolder extends RecyclerView.ViewHolder {
-        public TextView txtNom, txtCategorie, txtPrix, txtQtt;
+        public TextView tvCategorie, tvNom, tvPrix,tvQtt;
+
 
         public NoteHolder(@NonNull View itemView) {
             super(itemView);
-            txtNom = binding.t;
-            txtNom = binding.tvNom;
-            txtAdress = binding.tvAdresse;
+            tvCategorie = binding.categorieRangee;
+            tvNom = binding.nomRangee;
+            tvPrix = binding.prixRangee;
+            tvQtt = binding.qttRangee;
         }
     }
+
+
+//    public ObjetAdapteur(@NonNull Context context, ArrayList<Objet> m_RowModels) {
+//
+//        super(context, R.layout.vendeur_row, R.id.nom_rangee, m_RowModels);
+//        contexte = context;
+//        rowModels = m_RowModels;
+//    }
+//
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        ViewHolder viewHolder;
+//
+//            /* Android recycle les conteneurs destinés à l'affichage
+//               ici, convertView est une vue recyclée qui représente un conteneur de rangée (View row)
+//               on vérifie qu'elle existe - si non, alors on la crée. */
+//        if (convertView == null) {
+//            // On crée d'abord un LayoutInflater depuis le contexte pour cette classe
+//            // car on ne peut pas utiliser celui de notre activité (classe englobante)
+//            LayoutInflater inflater = (LayoutInflater) contexte.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            convertView = inflater.inflate(R.layout.vendeur_row, parent, false);
+//            // Création d'un ViewHolder
+//            viewHolder = new ViewHolder(convertView);
+//            // On le passe à la vue recyclée grâce à setTag
+//            convertView.setTag(viewHolder);
+//        }
+//        else { viewHolder = (ViewHolder) convertView.getTag(); }
+//
+//        // Recyclage d'une ligne à l'aide de la méthode de la classe mère.
+//        View row = super.getView(position, convertView, parent);
+//
+//        Objet rowData = rowModels.get(position);
+//        viewHolder.tvCategorie.setText(rowData.getCategorie());
+//        viewHolder.tvNom.setText(rowData.getNom());
+//        viewHolder.tvPrix.setText(rowData.getPrix().toString());
+//        viewHolder.tvQtt.setText(rowData.getQtt().toString());
+//
+//
+//        return row;
+//    }
+//
+//
+//    private static class ViewHolder {
+//        TextView tvCategorie;
+//        TextView tvNom;
+//        TextView tvPrix;
+//        TextView tvQtt;
+//
+//        /** ViewHolder
+//         *
+//         * @param view
+//         */
+//        public ViewHolder(View view) {
+//            tvCategorie = (TextView) view.findViewById(R.id.categorie_rangee);
+//            tvNom = (TextView) view.findViewById(R.id.nom_rangee);
+//            tvPrix = (TextView) view.findViewById(R.id.prix_rangee);
+//            tvQtt = (TextView) view.findViewById(R.id.qtt_rangee);
+//        }
+//    }
+
+
 }
